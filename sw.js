d57baa68265fb9798e0d53a3b638646e0b6403ed
mrbana69 <bana.emi2007@gmail.com>
2026-01-31 22:37:16 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,8 +17,38 @@ self.addEventListener('install', (event) => {
 // Fetch Event: Serve from cache if available, otherwise network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      if (event.request.url.includes('index.html')) {
+        // For index.html, check for updates
+        return fetch(event.request).then((networkResponse) => {
+          if (networkResponse.ok) {
+            // Compare cached and network versions
+            if (cachedResponse) {
+              Promise.all([cachedResponse.clone().text(), networkResponse.clone().text()]).then(([cachedText, networkText]) => {
+                if (cachedText !== networkText) {
+                  // Notify clients of update
+                  self.clients.matchAll().then((clients) => {
+                    clients.forEach((client) => {
+                      client.postMessage({ type: 'UPDATE_AVAILABLE' });
+                    });
+                  });
+                }
+              });
+            }
+            // Cache the new version
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+            });
+            return networkResponse;
+          } else if (cachedResponse) {
+            return cachedResponse;
+          }
+        }).catch(() => {
+          return cachedResponse;
+        });
+      } else {
+        return cachedResponse || fetch(event.request);
+      }
     })
   );
 });
