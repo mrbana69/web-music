a4687539e3bebf5ec0625d1a93b6f0ec382baa31
mrbana69 <bana.emi2007@gmail.com>
2026-09-08 15:50:44 +0200

    fix(deploy): remove obsolete api/lib catalog and add CORS headers to api/audio

--- api/audio.js
+++ api/audio.js
@@ -1,6 +1,15 @@
 const musicController = require('../controllers/musicController');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, Cookie, x-ytm-cookie, x-youtube-cookie');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   try {
     await musicController.audio(req, res, (err) => {
       if (err && !res.headersSent) {
@@ -13,3 +22,4 @@ module.exports = async (req, res) => {
     }
   }
 };
+
